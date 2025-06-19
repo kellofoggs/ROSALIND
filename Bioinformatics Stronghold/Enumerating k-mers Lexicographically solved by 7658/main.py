c8e4e74623b44ccbb006d1d8b1cb4f1debc93847
kellofoggs <kojukwu2003@gmail.com>
2025-06-18 19:59:29 -0700

    Finished RNA splicing and enumerating kmers lexicographically

---
+++ Bioinformatics Stronghold/Enumerating k-mers Lexicographically solved by 7658/main.py
@@ -0,0 +1,52 @@
+from typing import List
+import sys
+from Utilities.InputFileTools import GenericTextFile
+alphabet_string = "A C G T"
+k_mer_len = 2
+alphabet_list = alphabet_string.split()
+
+
+# First things first we make all the possible strings of length 1
+
+def make_lexographic_strings(alphabet_string:str, string_length=2):
+    k_mer_len = 2
+    alphabet_list = alphabet_string.split()
+
+    if string_length == 1:
+        return alphabet_list
+
+    output_list = len(alphabet_list)*[list()]
+    enumeration = make_string_recur(alphabet_list,alphabet_list, count=string_length, current_layer=0)
+    for word in enumeration:
+        print(word)
+
+    pass
+
+# def make_layer(alpha_bet_list):
+
+
+
+def make_string_recur(prev_layer:List,base_layer:List, count=0, current_layer=0):
+
+    
+    if current_layer == count - 1:
+        return prev_layer
+    
+    # prev_layer = []
+    curr_layer = []
+    for i in range(0, len(prev_layer)):
+        for j in range(0, len(base_layer)):
+            curr_layer.append(prev_layer[i] + base_layer[j])
+
+
+    next_layer = make_string_recur(curr_layer ,base_layer, count, current_layer+1)
+    
+    return next_layer
+
+    
+
+def main(alphabet_string:str, string_length:int):
+    make_lexographic_strings(alphabet_string, string_length)
+
+file_lines = GenericTextFile.get_generic_file_as_lines(fasta_file=sys.argv[1])
+main(alphabet_string=file_lines[0], string_length=int(file_lines[1]))
---
+++ Bioinformatics Stronghold/RNA Splicing/main.py
@@ -0,0 +1,18 @@
+from Utilities.DNA import StringTools
+from Utilities.InputFileTools import Fasta
+import sys
+import re
+from typing import List
+
+def main(dna_string:str, exons:List):
+    
+    # exons_translation = dict([(exon, " ") for exon in exons])
+    intron_pattern = f'({"|".join(exons)})'
+    exon_string = re.sub( intron_pattern, "", dna_string)
+    print(StringTools.translate_dna_to_amino_acid(exon_string))
+    # print(exons_translation)
+    # dna_string.translate(str.maketrans(exons_translation))
+
+fasta_file_path = sys.argv[1]
+dna_string, *exons = Fasta.get_fasta_as_list(fasta_file=fasta_file_path)
+main(dna_string, exons)
