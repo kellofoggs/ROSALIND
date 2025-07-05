acdd61e1377e17b3041e8bbe7aed51e627d35dd5
kellofoggs <kojukwu2003@gmail.com>
2025-07-04 21:24:26 -0700

    Started Bioinformatics Armory

---
+++ Bionformatics Armory/Introduction to the Bioinformatics Armory/main.py
@@ -0,0 +1,29 @@
+from Bio.Seq import Seq
+from Utilities.InputFileTools import GenericTextFile
+from Utilities.DNA import StringTools
+from Utilities.Solution import BaseSolution
+import sys
+import argparse
+
+
+class Solution(BaseSolution):
+    def __init__(self):
+        super().__init__()
+        
+    def solve(self):
+        dna_string = "".join(self.get_generic_input_file())
+        count_dict = StringTools.count_nucleoutides(dna_string)
+        nucs = ["A", "C", "G", "T"]
+
+        nuc_counts = list(map(lambda nuc: str(count_dict.get(nuc)), nucs ))
+        print(" ".join(nuc_counts))
+        
+        pass
+
+def main():
+
+    my_solution = Solution()
+    my_solution.solve()
+
+
+main()
